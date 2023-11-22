d12254d85c47cec8a57de8704a9d63dd49a3894d
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-11-22 16:29:21 -0300

    added delete books function

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
     const updatedBooks = [...books, { id: Math.round(Math.random() * 9999), title }];
     setBooks(updatedBooks);
@@ -12,7 +20,7 @@ function App() {
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
