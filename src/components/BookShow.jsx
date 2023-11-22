5a3b8ce96d8bd32117c7cd0d54d26c867c921cd0
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-11-22 17:35:30 -0300

    added bookEdit function

--- src/components/BookShow.jsx
+++ src/components/BookShow.jsx
@@ -1,13 +1,35 @@
-const BookShow = ({ book, onDelete }) => {
-  const handleClick = () => {
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
+const BookShow = ({ book, onDelete, onEdit }) => {
+  const [showEdit, SetShowEdit] = useState(false);
+
+  const handleEditClick = () => {
+    SetShowEdit(!showEdit);
+  };
+
+  const handleDeleteClick = () => {
     onDelete(book.id);
   };
 
+  const handleSubmit = (id, newTitle) => {
+    SetShowEdit(false);
+    onEdit(id, newTitle);
+  };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit book={book} onSubmit={handleSubmit} />;
+  }
+
   return (
     <div className="book-show">
-      {book.title}
+      <div>{content}</div>
       <div className="actions">
-        <button className="delete" onClick={handleClick}></button>
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}></button>
       </div>
     </div>
   );
